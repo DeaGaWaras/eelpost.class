5079adea6aaf6dfc89a9b74a34608b913f9b4b7c
Syah <asyaharadea@gmail.com>
2024-07-28 00:20:45 +0700

    v2.0.1

--- src/views/ScheduleView.vue
+++ src/views/ScheduleView.vue
@@ -1,16 +1,15 @@
 <template>
-  <div class="container mx-auto p-3 md:p-8">
+  <div class="container mx-auto p-3 md:p-8 mt-4 pb-10">
     <div class="flex flex-col-reverse md:flex-row relative">
       <div class="w-full md:w-2/3">
         <div class="flex flex-col gap-4 md:px-20 fade-zoom-up">
           <article v-for="article in articles" :key="article.id">
             <router-link :to="`/read/${article.slug}/${article.id}`" class="flex w-full bg-[#1e1e1f] border-[#383838] rounded-xl text-left text-white p-5 md:py-7 md:px-8 cursor-pointer hover:bg-[#282828] items-center">
               <div class="w-full pr-4">
-                <div class="text-xs mb-1 text-slate-400 flex items-center italic">
-                  <div class="h-[5px] w-20 bg-amber-200 md:w-5 aos-init aos-animate mr-2"></div> {{ article.name }}
-                </div>
-                <h1 class="text-sm md:text-md text-amber-200 font-bold mb-2 paraf">{{ article.title }}</h1>
-                <div class="text-sm hidden md:block paraf">{{ article.desc }}</div>
+                <div class="text-slate-400 flex items-center italic custom-text mb-1">
+                   <div class="h-[1px] w-20 bg-amber-200 md:w-5 aos-init aos-animate mr-2"></div> 
+                   {{ article.name }}
+                 </div>
               </div>
             </router-link>
           </article>
