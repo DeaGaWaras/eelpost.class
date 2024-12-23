8ed2d0862e6ce888bd97775d15e5126a48ab8055
RadeaSukaSusu <121932307+DeaGaWaras@users.noreply.github.com>
2024-12-23 21:54:10 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,8 @@
         </button>
         <div class="flex md:order-2 fadein-bot">
           <a href="https://www.instagram.com/belevens.24"><img class="w-9 rounded-full" src="https://telegra.ph/file/bbc21149a98d0f6268f84.png" alt="Instagram"></a>
-          <a href="https://www.tiktok.com/@belevens.24"><img class="w-9 rounded-full" src="https://img.icons8.com/ios-filled/500/tiktok--v1.png" alt="Tiktok"></a>
+          <br>
+          <a href="https://www.tiktok.com/@belevens.24"><img class="w-9 rounded-full" src="https://img.icons8.com/3d-fluency/750/tiktok-logo.png" alt="Tiktok"></a>
         </div>
         <div class="hidden md:flex justify-between items-center w-full md:w-auto md:order-1" id="mobile-menu-3">
           <ul class="flex-col md:flex-row flex md:space-x-8 mt-4 md:mt-0 md:text-sm md:font-medium">
